d3014a23b3fb2f888c15f07c3581c7f36584b38b
jfinley961 <justin.finley1@gmail.com>
2017-05-10 23:00:09 -0300

    Added Text Field and Button

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -1,7 +1,13 @@
 package com.acme.csci3130;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +15,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText2);
+        String message = editText.getText().toString();
+        TextView textView = (TextView) findViewById(R.id.helloText);
+        textView.setText(message);
     }
 }
